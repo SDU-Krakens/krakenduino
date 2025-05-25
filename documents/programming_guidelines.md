94ee0397f8f46bb13eb7583bf64fd6dba85ace38
AiwendilsCode <caban.milan04@gmail.com>
2025-05-25 18:08:31 +0200

    Update program template.

---
+++ documents/programming_guidelines.md
@@ -0,0 +1,5 @@
+## Formatting
+Use clang format extension in Visual studio.
+
+## Best practices
+Never use malloc or new.
--- software/template/src/main.c
+++ software/template/src/main.c
@@ -4,8 +4,14 @@
 #include <freertos/FreeRTOS.h>
 
 void app_main() {
+  esp_log_level_set("main.c", ESP_LOG_DEBUG); // Set the log level for all components to verbose
+
   for (;;) {
-    ESP_LOGI("TEST", "TEST");
+    ESP_LOGV("main.c", "Hello verbose!");
+    ESP_LOGD("main.c", "Hello debug!");
+    ESP_LOGI("main.c", "Hello information!");
+    ESP_LOGW("main.c", "Hello warning!");
+    ESP_LOGE("main.c", "Hello error!");
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
